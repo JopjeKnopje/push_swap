5f34bc90d5ea825e229a09fe629f676fe2a0d9ba
joppe <jboeve009@gmail.com>
2023-01-26 23:36:56 +0100

    can print the list now

--- src/main.c
+++ src/main.c
@@ -6,10 +6,9 @@
 /*   By: jboeve <marvin@42.fr>                        +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/26 16:06:53 by jboeve        #+#    #+#                 */
-/*   Updated: 2023/01/26 18:56:59 by joppe         ########   odam.nl         */
+/*   Updated: 2023/01/26 23:36:35 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "libft.h"
 #include "push_swap.h"
@@ -23,8 +22,14 @@ int is_num_str(char *s)
 	int i = 0;
 	while (s[i]) 
 	{
-		if (!ft_isdigit(s[i]))
-			return (0);
+
+		int num = ft_atoi(s);
+		if (num)
+		{
+			printf("num %d\n", num);
+		}
+		// if (!ft_isdigit(s[i]))
+		// 	return (0);
 		i++;
 	}
 	return (1);
@@ -39,38 +44,52 @@ int parse_args(char *argv[])
 	{
 		if (!is_num_str(argv[i]))
 			return (0);
+
 		i++;
 	}
 	return (1);
 }
-
+      
 t_list *create_stack_a(char *argv[])
 {
 	t_list *head = ft_lstnew(argv[1]);
-	return head;
+
+	int i = 1;
+	while (argv[i])
+	{
+		ft_lstadd_front(&head, ft_lstnew(argv[i]));
+		// ft_lstadd_back(&head, ft_lstnew(argv[i]));
+		i++;
+	}
+	return (head);
 }
-
-
 
 void print_list(t_list *head)
 {
-	t_list *tmp;
+	t_list *tmp = head;
 	while (tmp)
 	{
-	
+		if (!tmp->next)
+			break ;
+
+		printf("%s\n", (char *) tmp->content);
+
+		tmp = tmp->next;
 	}
 }
 
 int main (int argc, char *argv[])
 {
-	if (argc > 1 && parse_args(argv))
-	{
-		// add to list
-		t_list *head = create_stack_a(argv);
-	}
-	else
-		printf("Error!\n");
-
-	print_list(head);
+	parse_args(argv);
+	// t_list *head_a;
+	// if (argc > 1 && parse_args(argv))
+	// {
+	// 	// add to list
+	// 	 head_a = create_stack_a(argv);
+	// }
+	// else
+	// 	printf("Error!\n");
+	//
+	// print_list(head_a);
 	return 0;
 }
