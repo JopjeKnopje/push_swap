f58a0902506b5df2d31fa564be6e84ec003ed98a
JopjeKnopje <jboeve009@gmail.com>
2023-02-02 16:32:51 +0100

    started integer overflow protection

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <marvin@42.fr>                        +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/01/26 16:06:53 by jboeve        #+#    #+#                 */
-/*   Updated: 2023/01/31 21:24:30 by joppe         ########   odam.nl         */
+/*   Updated: 2023/02/02 16:20:03 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,14 @@ int parse_args(char *argv[])
       
 t_node *create_stack_a(char *argv[])
 {
-	t_node *head = stack_new(ft_atoi(argv[1]));
+	// t_node *head = stack_new(ft_atoi(argv[1]));
+	t_node *head;
+	long num = atol(argv[1]);
 
 	int i = 2;
 	while (argv[i])
 	{
-		int num = ft_atoi(argv[i]);
+		num = atol(argv[1]);
 		stack_add_front(&head, stack_new(num));
 		// stack_add_back(&head, stack_new(num));
 		i++;
